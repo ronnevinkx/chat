028ea9f43e4d7649be2fa0d3c0b0e01b595e84d4
ronnevinkx <ronnevinkx@gmail.com>
2021-06-09 15:35:51 +0200

    Testing - added first test

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+function Button({ label }) {
+	return <button data-testid="btn-primary">{label}</button>;
+}
+
+// clean up dom after each test
+afterEach(cleanup);
+
+describe('App', () => {
+	describe('Initialization', () => {
+		it('renders without crashing', () => {
+			ReactDOM.render(<Button label="Click me" />, document.createElement('div'));
+		});
+
+		it('renders button correctly with "Click me"', () => {
+			const { getByTestId } = render(<Button label="Click me" />);
+			expect(getByTestId('btn-primary')).toHaveTextContent('Click me');
+		});
+
+		it('renders button correctly with "Click again"', () => {
+			const { getByTestId } = render(<Button label="Click again" />);
+			expect(getByTestId('btn-primary')).toHaveTextContent('Click again');
+		});
+	});
+});
